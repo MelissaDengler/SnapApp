79095015fea5acc7b5b98a9656300faf82becf37
Melissa <mel.dengler@gmail.com>
2025-02-01 19:44:08 +0200

    added cofig

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_API_URL: string;
+  readonly VITE_ENABLE_ANIMATIONS: string;
 }
 
 interface ImportMeta {
